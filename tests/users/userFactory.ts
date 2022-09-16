8176a2f2cc78d978c4adc1fd0142439fec5b1b3e
brualvess <oliveirabrunna2210@gmail.com>
2022-09-16 16:48:34 -0300

    test route post /signin

--- tests/users/userFactory.ts
+++ tests/users/userFactory.ts
@@ -1,14 +1,9 @@
 import { faker } from '@faker-js/faker';
 
-export interface IUser {
-    email: string,
-    password: string,
-    confirmPassword: string
-}
 
 export async function userFactory() {
     const password = faker.internet.password()
-    const obj:IUser =  {
+    const obj =  {
         email: faker.internet.email(),
         password: password,
         confirmPassword: password
--- tests/users/users.test.ts
+++ tests/users/users.test.ts
@@ -1,7 +1,7 @@
 import supertest from 'supertest';
 import app from '../../src/index'
 import { prisma } from '../../src/database';
-import { userFactory, IUser } from './userFactory'
+import { userFactory } from './userFactory'
 
 
 describe('Testa POST/signup ', () => {
@@ -27,15 +27,33 @@ describe('Testa POST/signup ', () => {
 
     it('Deve retornar 422, caso falte enviar algum campo do body', async () => {
         let datas = await userFactory();
-        let response = {...datas,password: undefined}
+        let response = { ...datas, password: undefined }
         let result = await supertest(app).post(`/signup`).send(response);
 
         expect(result.status).toBe(422);
 
-        response = {...datas,email: undefined}
-         result = await supertest(app).post(`/signup`).send(response);
-        
+        response = { ...datas, email: undefined }
+        result = await supertest(app).post(`/signup`).send(response);
+
         expect(result.status).toBe(422);
+    })
+
+})
+describe('Testa POST/signin ', () => {
+    it('Deve retornar 200 caso faça login', async () => {
+        let datas = await userFactory();
+        await supertest(app).post('/signup').send(datas);
+
+        datas = { ...datas, confirmPassword: undefined }
+        const result = await supertest(app).post(`/signin`).send(datas);
+        expect(result.status).toBe(200)
+        expect(result.body).not.toBeNull();
+    })
+
+    it('deve retornar 401 caso informe usuário ou senha inválido', async()=>{
+        let datas = await userFactory();
+        datas = { ...datas, confirmPassword: undefined }
+        await supertest(app).post(`/signin`).send(datas)
     })
 
 })
