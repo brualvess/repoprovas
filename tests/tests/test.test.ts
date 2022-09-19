08a250595537450fa056b189a642dd8ec9330fe8
brualvess <oliveirabrunna2210@gmail.com>
2022-09-19 10:40:36 -0300

    test get /test

--- tests/tests/test.test.ts
+++ tests/tests/test.test.ts
@@ -2,8 +2,7 @@ import supertest from 'supertest';
 import app from '../../src/index'
 import { prisma } from '../../src/database';
 import {
-    testFactory,
-    testFactory2
+    testFactory
 } from './testFactory'
 import { userFactory } from '../users/userFactory'
 
@@ -38,7 +37,38 @@ describe('Testa POST/test ', () => {
 
         const datas = await testFactory()
         const result = await supertest(app).post('/test').set({ Authorization: `Beare ${token}` }).send(datas);
-    
+
         expect(result.status).toBe(403);
+    })
+})
+
+ describe('Testa GET/testTeacher ', () => {
+     it('Deve retornar 200 e o body com as provas organizada por instrutores', async () => {
+        let datasUser = await userFactory();
+        await supertest(app).post('/signup').send(datasUser);
+        datasUser = { ...datasUser, confirmPassword: undefined }
+        const response = await supertest(app).post(`/signin`).send(datasUser);
+        const token = response.text
+
+      const createTest = {
+        name : 'DrivenEats',
+        pdfUrl: 'driven@eats.com',
+        categoryId: 1,
+        teacherDisciplineId: 1,
+      }
+      const createTest2 = {
+        name : 'ZapRecall',
+        pdfUrl: 'zap@recall.com',
+        categoryId: 1,
+        teacherDisciplineId: 2,
+      }
+      await supertest(app).post('/test').set({ Authorization: `Bearer ${token}` }).send(createTest);
+     await supertest(app).post('/test').set({ Authorization: `Bearer ${token}` }).send(createTest2);
+
+    const result = await supertest(app).get(`/testTeacher`).set({ Authorization: `Bearer ${token}` });
+
+    expect(result.status).toBe(200);
+    expect(result.body).toBeInstanceOf(Object);
+ 
     })
 })
--- tests/tests/testFactory.ts
+++ tests/tests/testFactory.ts
@@ -8,11 +8,3 @@ export async function testFactory() {
         teacherDisciplineId: 1,
     }
 }
-export async function testFactory2(){
-   return {
-    name: faker.lorem.word(10),
-    pdfUrl: faker.internet.url(),
-    categoryId: -1,
-    teacherDisciplineId: 1,
-   } 
-}
