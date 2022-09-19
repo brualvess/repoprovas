9d84f8974eaee651dab5660829335aed84c265ac
brualvess <oliveirabrunna2210@gmail.com>
2022-09-19 11:33:38 -0300

    test get /testDiscipline

--- tests/tests/test.test.ts
+++ tests/tests/test.test.ts
@@ -9,6 +9,18 @@ import { userFactory } from '../users/userFactory'
 beforeEach(async () => {
     await prisma.$executeRaw`TRUNCATE TABLE "tests"`;
 });
+const createTest = {
+    name : 'DrivenEats',
+    pdfUrl: 'driven@eats.com',
+    categoryId: 1,
+    teacherDisciplineId: 1,
+  }
+  const createTest2 = {
+    name : 'ZapRecall',
+    pdfUrl: 'zap@recall.com',
+    categoryId: 1,
+    teacherDisciplineId: 2,
+  }
 describe('Testa POST/test ', () => {
     it('Deve retornar 201 caso todos os dados estejam corretos', async () => {
         let datasUser = await userFactory();
@@ -50,18 +62,7 @@ describe('Testa POST/test ', () => {
         const response = await supertest(app).post(`/signin`).send(datasUser);
         const token = response.text
 
-      const createTest = {
-        name : 'DrivenEats',
-        pdfUrl: 'driven@eats.com',
-        categoryId: 1,
-        teacherDisciplineId: 1,
-      }
-      const createTest2 = {
-        name : 'ZapRecall',
-        pdfUrl: 'zap@recall.com',
-        categoryId: 1,
-        teacherDisciplineId: 2,
-      }
+     
       await supertest(app).post('/test').set({ Authorization: `Bearer ${token}` }).send(createTest);
      await supertest(app).post('/test').set({ Authorization: `Bearer ${token}` }).send(createTest2);
 
@@ -73,4 +74,27 @@ describe('Testa POST/test ', () => {
     })
 
     
+})
+
+
+describe('Testa GET/testDiscipline ', () => {
+    it('Deve retornar 200 e o body com as provas organizada por disciplina', async () => {
+        let datasUser = await userFactory();
+        await supertest(app).post('/signup').send(datasUser);
+        datasUser = { ...datasUser, confirmPassword: undefined }
+        const response = await supertest(app).post(`/signin`).send(datasUser);
+        const token = response.text
+
+     
+      await supertest(app).post('/test').set({ Authorization: `Bearer ${token}` }).send(createTest);
+     await supertest(app).post('/test').set({ Authorization: `Bearer ${token}` }).send(createTest2);
+
+    const result = await supertest(app).get(`/testDiscipline`).set({ Authorization: `Bearer ${token}` });
+
+    expect(result.status).toBe(200);
+    expect(result.body).toBeInstanceOf(Object);
+    })
+      
+
+   
 })
